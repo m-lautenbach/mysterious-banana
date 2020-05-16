03fe189a0b719a9b208b5e4f07a45d4985b35038
Markus Lautenbach <markus.lautenbach@actano.de>
2020-05-16 15:50:55 +0200

    Move implementation out of testfile

--- src/__tests__/integerToArray.js
+++ src/__tests__/integerToArray.js
@@ -1,20 +1,4 @@
-// Take 16 bit unsigned integer and return array of bit values, most significant at the left
-const integerToArray = (canvasRowAsInteger, length = 16) => {
-  // throwing exception for invalid numbers for now, as it's unclear how they should be handled (fail early)
-  // proper handling would probably be too much work for the scope of this exercise
-  if (canvasRowAsInteger % 1 !== 0) {
-    throw new TypeError(`number ${canvasRowAsInteger} should be an integer`)
-  }
-  if (canvasRowAsInteger >= Math.pow(2, length) || canvasRowAsInteger < 0) {
-    throw new RangeError(`integer ${canvasRowAsInteger} can not be mapped to a bit array of length ${length}`)
-  }
-  return Array(length).fill(0).map((_, index) => {
-    // array are indexed from the left, but we want the least significant bit on the right
-    const indexFromRight = length - 1 - index
-    const filter = 1 << indexFromRight
-    return (canvasRowAsInteger & filter) ? 1 : 0
-  })
-}
+import { integerToArray } from '../conversions'
 
 describe('conversion of an unsigned integer representing 16 bits to an array', () => {
   test('for 0 it should return an array full of 0s', () => {
---
+++ src/conversions.js
@@ -0,0 +1,18 @@
+// Take 16 bit unsigned integer and return array of bit values, most significant at the left
+export const integerToArray = (canvasRowAsInteger, length = 16) => {
+  // throwing exception for invalid numbers for now, as it's unclear how they should be handled (fail early)
+  // proper handling would probably be too much work for the scope of this exercise
+  if (canvasRowAsInteger % 1 !== 0) {
+    throw new TypeError(`number ${canvasRowAsInteger} should be an integer`)
+  }
+  if (canvasRowAsInteger >= Math.pow(2, length) || canvasRowAsInteger < 0) {
+    throw new RangeError(`integer ${canvasRowAsInteger} can not be mapped to a bit array of length ${length}`)
+  }
+  return Array(length).fill(0).map((_, index) => {
+    // array are indexed from the left, but we want the least significant bit on the right
+    const indexFromRight = length - 1 - index
+    const mask = 1 << indexFromRight
+    // if selected bit is not set, the application of the mask will return zero, otherwise > zero
+    return (canvasRowAsInteger & mask) ? 1 : 0
+  })
+}
